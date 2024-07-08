4a1fcd0a35c0f4f40ead9e454f013042eeebc117
servostar <sven.vogel123@web.de>
2024-07-08 22:56:56 +0200

    changed: CI runs on nix shell

---
+++ shell.nix
@@ -0,0 +1,13 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    python312
+    python312Packages.pyyaml
+    typst
+    typstyle
+  ];
+}
