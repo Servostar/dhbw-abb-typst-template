946a16fce6a0541723bfadf80fe8e7b77431a354
servostar <sven.vogel123@web.de>
2024-08-27 19:50:31 +0200

    add typstyle check to CI (#13)

--- run-ci.sh
+++ run-ci.sh
@@ -34,6 +34,7 @@ function enter-section() {
   log "INFO" "section $1 completed successfully"
 }
 
+enter-section "Typstyle checking" "./run-fmt.sh --check src/lib.typ" 0
 enter-section "BUILD: ABB code theme" "./generate-theme.sh" 0
 enter-section "Compiling template..." "typst compile template/main.typ --root . example.pdf" 0
 enter-section "TEST: local template import" "typst compile tests/local-import/main.typ --root ." 0
